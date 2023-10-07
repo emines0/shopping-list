0f53e4299a9f1e05cbfec6c46fb672e38856fef3
emines0 <emin.sefa1@gmail.com>
2023-10-07 11:57:35 +0200

    remove from local storage, update item

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const itemList = document.getElementById('item-list')
 const clearBtn = document.getElementById('clear')
 const itemFilter = document.getElementById('filter')
 const items = itemList.querySelectorAll('li')
+const formBtn = itemForm.querySelector('button')
+
+let isEditMode = false
 
 // Functions
 
@@ -21,6 +24,41 @@ function createIcon(classes) {
     return icon
 }
 
+function addItemToStorage(item) {
+    const itemsFromStorage = getItemsFromStorage()
+
+    // Add the new item to the array of the itemsFromStorage
+    itemsFromStorage.push(item)
+
+    // Convert to JSON string and set to local storage
+    localStorage.setItem('items', JSON.stringify(itemsFromStorage))
+}
+
+function getItemsFromStorage() {
+    let itemsFromStorage
+
+    // Check if there is something in local storage
+    if (localStorage.getItem('items') === null) {
+        // If not then set itemsFromStorage to an empty array
+        itemsFromStorage = []
+    } else {
+        /* If yes the set itemsFromStorage to array with items from local storage. 
+           Data from local storage coming in JSON string we have to parse them to the array 
+           with JSON.parse() method */
+        itemsFromStorage = JSON.parse(localStorage.getItem('items')) // parse string to array
+    }
+
+    return itemsFromStorage
+}
+
+function displayItems() {
+    const itemsFromStorage = getItemsFromStorage()
+
+    itemsFromStorage.forEach((item) => addItemToDOM(item))
+
+    checkUI()
+}
+
 function onAddItemSubmit(e) {
     e.preventDefault()
 
@@ -30,6 +68,16 @@ function onAddItemSubmit(e) {
     if (newItem === '') {
         alert('Please add an item')
         return
+    }
+
+    // Check for edit mode
+    if (isEditMode) {
+        const itemToEdit = itemList.querySelector('.edit-mode')
+
+        removeItemFromStorage(itemToEdit.textContent)
+        itemToEdit.classList.remove('edit-mode')
+        itemToEdit.remove()
+        isEditMode = false
     }
 
     // Create item DOM element
@@ -55,35 +103,57 @@ function addItemToDOM(item) {
     itemList.appendChild(li)
 }
 
-function addItemToStorage(item) {
-    let itemsFromStorage
-
-    // Check if there is something in local storage
-    if (localStorage.getItem('items') === null) {
-        // If not then set itemsFromStorage to an empty array
-        itemsFromStorage = []
+function onClickItem(e) {
+    // Check if user clicks on remove button icon
+    if (e.target.parentElement.classList.contains('remove-item')) {
+        removeItem(e.target.parentElement.parentElement)
     } else {
-        /* If yes the set itemsFromStorage to array with items from local storage. 
-           Data from local storage coming in JSON string we have to parse them to the array 
-           with JSON.parse() method */
-        itemsFromStorage = JSON.parse(localStorage.getItem('items')) // parse string to array
+        setItemToEdit(e.target)
     }
-
-    // Add the new item to the array of the itemsFromStorage
-    itemsFromStorage.push(item)
-
-    // Convert to JSON string and set to local storage
-    localStorage.setItem('items', JSON.stringify(itemsFromStorage))
 }
 
-function removeItem(e) {
-    if (e.target.parentElement.classList.contains('remove-item')) {
-        // Check if user clicks on remove button icon
-        if (confirm('Are you sure?')) {
-            e.target.parentElement.parentElement.remove() // remove whole list item parent,parent element of an remove button icon
-        }
-        checkUI()
+function setItemToEdit(item) {
+    isEditMode = true
+
+    // Remove edit-mode class from other list items
+    itemList
+        .querySelectorAll('li')
+        .forEach((listItem) => listItem.classList.remove('edit-mode'))
+
+    // Lighter color of edited item
+    item.classList.add('edit-mode')
+    // Change icon/text/color of form btn
+    formBtn.innerHTML = '<i class="fas fa-solid fa-pen"></i> Update Item '
+    formBtn.style.backgroundColor = '#228B22'
+    // Set text content of edited item to the input field
+    itemInput.value = item.textContent
+
+    // Update clicked item
+    updateItem()
+}
+
+function updateItem(item) {}
+
+function removeItem(item) {
+    if (confirm('Are you sure?')) {
+        // remove item from DOM
+        item.remove()
+
+        // remove item from LocalStorage
+        // don't need whole element just his text content
+        removeItemFromStorage(item.textContent)
     }
+    checkUI()
+}
+
+function removeItemFromStorage(itemText) {
+    let itemsFromStorage = getItemsFromStorage()
+
+    // Filter out item to be removed
+    itemsFromStorage = itemsFromStorage.filter((item) => item !== itemText)
+
+    // Reset to LocalStorage
+    localStorage.setItem('items', JSON.stringify(itemsFromStorage))
 }
 
 function clearItems(e) {
@@ -91,6 +161,10 @@ function clearItems(e) {
         // while itemList has first child
         itemList.removeChild(itemList.firstChild) // remove first child from itemList
     }
+
+    // Clear from LocalStorage
+    localStorage.removeItem('items')
+
     checkUI()
 }
 
@@ -111,6 +185,8 @@ function filterItems(e) {
 }
 
 function checkUI() {
+    itemInput.value = ''
+
     const items = itemList.querySelectorAll('li')
 
     if (items.length === 0) {
@@ -120,13 +196,25 @@ function checkUI() {
         clearBtn.style.display = 'block'
         itemFilter.style.display = 'block'
     }
+
+    formBtn.innerHTML = '<i class="fas fa-solid fa-plus"></i> Add Item'
+    formBtn.style.backgroundColor = '#333'
+    isEditMode = false
 }
 
-// Event Listeners
-itemForm.addEventListener('submit', onAddItemSubmit)
-itemList.addEventListener('click', removeItem)
-clearBtn.addEventListener('click', clearItems)
-itemFilter.addEventListener('input', filterItems)
+// Initialize app
+function init() {
+    // Event Listeners
+    itemForm.addEventListener('submit', onAddItemSubmit)
+    itemList.addEventListener('click', onClickItem)
+    clearBtn.addEventListener('click', clearItems)
+    itemFilter.addEventListener('input', filterItems)
+
+    // Show items from local storage when DOM is loaded
+    document.addEventListener('DOMContentLoaded', displayItems)
+
+    checkUI()
+}
 
 // Global Scope
-checkUI()
+init()
